153081bb0cb0174733ba248e6db142aaa367273d
Amadou Mbengue <ambengueisidk@groupeisi.com>
2021-02-23 15:52:17 +0000

    new

--- src/main/java/sn/Am/domain/Infirmier.java
+++ src/main/java/sn/Am/domain/Infirmier.java
@@ -2,6 +2,6 @@ package sn.Am.domain;
 
 public class Infirmier {
     private int idInf;
-    private String user;
-    private String constante;
+    private User user;
+    private Constante constante;
 }
